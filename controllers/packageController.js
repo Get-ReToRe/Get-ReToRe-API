9c5781b9a5051a076b3794cd68c64cd930d3241a
Muhammad Tatma <muhammadtatma04@gmail.com>
2023-06-11 12:55:07 +0700

    fix package

--- controllers/packageController.js
+++ controllers/packageController.js
@@ -1,26 +1,26 @@
-let dummy = require('./paketdummy');
+const dummy = require('./paketdummy');
 
 module.exports = {
   getAllPackage : (req, res) => {
     const city = req.query.city
-    
+    let data = dummy;
     if(city){
-      dummy = dummy.filter( (package) => {
+      data  = data.filter( (package) => {
         return package.City.toLowerCase().includes(city.toLowerCase())
       })
     }
-    res.send(dummy)
+    res.send(data)
   },
 
   getPackageById : (req,res) => {
     const id = req.params.id
 
     if(id<=0 || id>dummy.length){
-      return res.send({success: false, message: "id not found"})
+      return res.send({success: false, message: `package with id ${id} not found`})
     }
 
     
-
+  
     res.send(dummy[id-1])
   }
 
