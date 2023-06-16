cfe8875af398edf9536d3caf620604e8c7d28da1
Muhammad Tatma <muhammadtatma04@gmail.com>
2023-06-16 18:32:03 +0700

    add swagger

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
-
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./Docs/openapi.json');
+var cors = require('cors')
 const app = express();
 
 // router
@@ -8,8 +10,11 @@ const recommendation = require('./routes/recommendationRoute');
 const placeRoute = require("./routes/placeRoute")
 const packageRoute = require('./routes/packageRoute')
 
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(helloRouter);
 app.use(recommendation);
